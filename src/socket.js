54a859b7c89b2dfca582175fbbb76d6362273adc
Evangel <dev.evangel@gmail.com>
2022-04-24 00:58:03 +0100

    Added socket url

--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,5 @@
 import io from "socket.io-client";
-const sockets = io("http://localhost:8000", {
+const sockets = io("https://ancient-retreat-51398.herokuapp.com", {
   autoConnect: true,
   forceNew: true,
 });
